6246d2338dd8a2fe7ebaf7f0899be63ec8c45dee
Bryan D <jules.bryand@gmail.com>
2016-11-11 15:53:25 -0500

    add: java library that provides jokes to button press

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import io.github.deltajulio.JokeClass;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +41,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeClass jokeClass = new JokeClass();
+        Toast.makeText(this, jokeClass.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ FinalProject/jokelib/src/main/java/io/github/deltajulio/JokeClass.java
@@ -0,0 +1,9 @@
+package io.github.deltajulio;
+
+public class JokeClass
+{
+    public String getJoke()
+    {
+        return "Bazinga";
+    }
+}
